8c393ec9b90ddcbc5eff897fc470781e11fc72e4
Hans Geel <mcmacker4@gmail.com>
2019-09-30 11:07:20 +0200

    Initial commit
    Project config

---
+++ webpack/common.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: "./src/main.ts",
+    output: {
+        path: path.resolve(__dirname, "../dist"),
+        filename: "playground.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.ts/,
+                loader: 'babel-loader!ts-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".ts", ".json", "*"]
+    },
+    plugins: [
+        new HtmlWebpackPlugin()
+    ]
+}
